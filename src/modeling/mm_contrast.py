c6ed69719f7d7c7fc6434a87bb6ddec1b5f47b80
Victor Machado <vmachado@pop-os.localdomain>
2023-08-18 19:44:39 -0300

    atu

--- src/modeling/mm_contrast.py
+++ src/modeling/mm_contrast.py
@@ -20,23 +20,23 @@ class AudioTextContrastive(nn.Module):
 
         self.mods_proj = nn.Sequential(
             nn.Dropout(p=rate), 
-            nn.Linear(wide_proj, wide_proj), 
+            nn.Linear(hidden_size, hidden_size), 
             nn.GELU(), 
-            nn.Linear(wide_proj, wide_proj)
+            nn.Linear(hidden_size, wide_proj)
         )
 
         self.text_proj = nn.Sequential(
             self.text_encoder, 
             nn.Linear(in_features_text, hidden_size),  
             nn.GELU(), 
-            nn.Linear(hidden_size, wide_proj),
+            nn.Linear(hidden_size, hidden_size),
             self.mods_proj,
         )
 
         self.audio_proj = nn.Sequential(
             nn.Linear(in_features_audio, hidden_size), 
             nn.GELU(), 
-            nn.Linear(hidden_size, wide_proj),
+            nn.Linear(hidden_size, hidden_size),
             self.mods_proj,
         )
 
@@ -44,9 +44,9 @@ class AudioTextContrastive(nn.Module):
         
         self.alpha = nn.Sequential(
             nn.Dropout(p=rate),
-            nn.Linear(wide_proj, 1)
+            nn.Linear(2 * wide_proj, 2)
         )
-        
+        self.wide_proj = wide_proj
         self.rate = rate
         
     def forward(self, inp):
@@ -76,22 +76,21 @@ class AudioTextContrastive(nn.Module):
         
         # Approximate text and audio, and make sum of vectors point to correct cls
         if multimodal != None:
-            x_mult_text = F.normalize(self.text_proj(multimodal['sentences']), dim=-1)
-            x_mult_text_alpha = self.alpha(x_mult_text)
+            x_mult_text = self.text_proj(multimodal['sentences'])
             
             x_mult_audio, _ = self.audio_encoder(**multimodal['audio_input'])
-            x_mult_audio = F.normalize(self.audio_proj(x_mult_audio), dim=-1)
-            x_mult_audio_alpha = self.alpha(x_mult_audio)
+            x_mult_audio = self.audio_proj(x_mult_audio)
             
-            alphas = F.softmax(torch.cat([x_mult_text_alpha, x_mult_audio_alpha], dim=-1), dim=-1).unsqueeze(dim=1)
+            alphas = self.alpha(torch.cat([x_mult_text, x_mult_audio], dim=-1))
+            alphas = F.softmax(alphas, dim=-1).unsqueeze(dim=1)
 
             # View 1
-            x_mult_text_wide = alphas[:, :, 1] * x_mult_text
-            x_mult_text = alphas[:, :, 1] * F.normalize(self.linear(x_mult_text), dim=-1)
+            x_mult_text_wide = alphas[:, :, 0] * F.normalize(x_mult_text, dim=-1)
+            x_mult_text = alphas[:, :, 0] * F.normalize(self.linear(x_mult_text), dim=-1)
             
             # View 2
-            x_mult_audio_wide = alphas[:, :, 0] * x_mult_audio
-            x_mult_audio = alphas[:, :, 0] * F.normalize(self.linear(x_mult_audio), dim=-1)
+            x_mult_audio_wide = alphas[:, :, 1] * F.normalize(x_mult_audio, dim=-1)
+            x_mult_audio = alphas[:, :, 1] * F.normalize(self.linear(x_mult_audio), dim=-1)
 
         return {
             "x_text": x_text,
